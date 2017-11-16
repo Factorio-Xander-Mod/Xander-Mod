dcfe016be6b846ac207c17c47184475d57e7a17d
jodokus31 <33641866+jodokus31@users.noreply.github.com>
2017-11-16 21:21:31 +0100

    Fix migrations
    Try to fix the migration error, when loading a save without xander-mod.
    - Do null checks for all accessed fields.
    - add game.reload_script and call reset functions

--- migrations/1-0-1_1-1-0.lua
+++ migrations/1-0-1_1-1-0.lua
@@ -1,7 +1,22 @@
+game.reload_script()
+
 for index, force in pairs(game.forces) do
+  force.reset_recipes()
+  force.reset_technologies()
+
   local technologies = force.technologies
   local recipes = force.recipes
-  recipes["steel-charge-a"].enabled = technologies["steel-processing"].researched
-  recipes["science-pack-1"].enabled = technologies["electricity"].researched
-  recipes["science-pack-1"].enabled = technologies["inserter-a"].researched
+
+  if recipes["steel-charge-a"] and technologies["steel-processing"] then
+    recipes["steel-charge-a"].enabled = technologies["steel-processing"].researched
+  end
+
+  if recipes["science-pack-1"] and technologies["electricity"] then
+    recipes["science-pack-1"].enabled = technologies["electricity"].researched
+  end
+
+  if recipes["science-pack-1"] and technologies["inserter-a"] then
+    recipes["science-pack-1"].enabled = technologies["inserter-a"].researched
+  end
+  
 end
--- migrations/1-3-1_1-3-2.lua
+++ migrations/1-3-1_1-3-2.lua
@@ -1,9 +1,26 @@
+game.reload_script()
+
 for index, force in pairs(game.forces) do
+  force.reset_recipes()
+  force.reset_technologies()
+
 	local technologies = force.technologies
 	local recipes = force.recipes
-	
-	recipes["piston-cylinder"].enabled = technologies["plumbing-steel"].researched
-	recipes["coil-1-b"].enabled = technologies["induction_2"].researched
-	recipes["inserter-slow"].enabled = technologies["inserter-a"].researched
-	recipes["inserter"].enabled = technologies["inserter-b"].researched
+
+	if recipes["piston-cylinder"] and technologies["plumbing-steel"] then
+		recipes["piston-cylinder"].enabled = technologies["plumbing-steel"].researched
+	end
+
+	if recipes["coil-1-b"] and technologies["induction_2"] then
+		recipes["coil-1-b"].enabled = technologies["induction_2"].researched
+	end
+
+	if recipes["inserter-slow"] and technologies["inserter-a"] then
+		recipes["inserter-slow"].enabled = technologies["inserter-a"].researched
+	end
+
+	if recipes["inserter"] and technologies["inserter-b"] then
+		recipes["inserter"].enabled = technologies["inserter-b"].researched
+	end
+
 end
