9828625979ca4fcf041001314ca3bba26b8811e9
GeekinaCave <jarelton85@gmail.com>
2017-11-23 20:07:51 -0500

    Changing "Iron Standard Inserter" to "Steel Standard Inserter" as prerequisite of "Logistics"
    since this unlock Green Science, but it uses "Standard Inserter" in his recipe

--- prototypes/technology/logistic.lua
+++ prototypes/technology/logistic.lua
@@ -14,7 +14,7 @@ data.raw.technology["electric-energy-distribution-2"].order = "06-01"
 --Base "logistics"
 data.raw.technology["logistics"].effects = {{type = "unlock-recipe", recipe = "transport-belt"}, 
 	{type = "unlock-recipe", recipe = "underground-belt"}, {type = "unlock-recipe", recipe = "splitter"}, {type = "unlock-recipe", recipe = "science-pack-2"}}
-data.raw.technology["logistics"].prerequisites = {"parts_1", "logistics_0", "inserter-a"}
+data.raw.technology["logistics"].prerequisites = {"parts_1", "logistics_0", "inserter-b"}
 data.raw.technology["logistics"].order = "06-04"
 
 --Base "logistics-2"
