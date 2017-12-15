af75dd6b6486f0091c4a21a576c4520264c61e62
mspielberg <9729801+mspielberg@users.noreply.github.com>
2017-12-15 15:27:27 -0800

    Fix robot.lua entities for 0.16

--- prototypes/entity/logistic/robot.lua
+++ prototypes/entity/logistic/robot.lua
@@ -1,3 +1,4 @@
+local xmutil = require("xmutil")
 
 --Base Overrides
 
@@ -310,143 +311,49 @@ data:extend(
 },
 --Base "roboport" placeholder
 --Long-Range Roboport
+xmutil.clone("roboport", "roboport",
+{},
 {
-	type = "roboport",
 	name = "roboport-2",
 	icon = "__xander-mod__/graphics/item/logistic/robot/roboport-2.png",
-	flags = {"placeable-player", "player-creation"},
 	minable = {hardness = 0.2, mining_time = 0.5, result = "roboport-2"},
 	max_health = 1000,
-	corpse = "big-remnants",
-	collision_box = {{-1.7, -1.7}, {1.7, 1.7}},
-	selection_box = {{-2, -2}, {2, 2}},
-	resistances =
-	{
-		{
-			type = "fire",
-			percent = 60
-		},
-		{
-			type = "impact",
-			percent = 30
-		}
-	},
-	dying_explosion = "medium-explosion",
-	energy_source =
-	{
-		type = "electric",
-		usage_priority = "secondary-input",
+	energy_source = {
 		input_flow_limit = "10MW",
 		buffer_capacity = "200MJ"
 	},
-	recharge_minimum = "40MJ",
 	energy_usage = "100kW",
 	charging_energy = "2MW",
 	logistics_radius = 50,
 	construction_radius = 110,
-	charge_approach_distance = 5,
 	robot_slots_count = 10,
 	material_slots_count = 10,
-	stationing_offset = {0, 0},
-	charging_offsets =
-	{
-		{-1.5, -0.5}, {1.5, -0.5}, {1.5, 1.5}, {-1.5, 1.5},
-	},
-	base =
-	{
-		filename = "__xander-mod__/graphics/entity/logistic/roboport-2/base.png",
-		width = 143,
-		height = 135,
-		shift = {0.5, 0.25}
-	},
-	base_patch =
-	{
-		filename = "__xander-mod__/graphics/entity/logistic/roboport-2/base-patch.png",
-		priority = "medium",
-		width = 69,
-		height = 50,
-		frame_count = 1,
-		shift = {0.03125, 0.203125}
-	},
-	base_animation =
-	{
-		filename = "__xander-mod__/graphics/entity/logistic/roboport-2/base-animation.png",
-		priority = "medium",
-		width = 42,
-		height = 31,
-		frame_count = 8,
-		animation_speed = 0.5,
-		shift = {-0.5315, -1.9375}
-	},
-	door_animation_up =
-	{
-		filename = "__xander-mod__/graphics/entity/logistic/roboport-2/door-up.png",
-		priority = "medium",
-		width = 52,
-		height = 20,
-		frame_count = 16,
-		shift = {0.015625, -0.890625}
-	},
-	door_animation_down =
-	{
-		filename = "__xander-mod__/graphics/entity/logistic/roboport-2/door-down.png",
-		priority = "medium",
-		width = 52,
-		height = 22,
-		frame_count = 16,
-		shift = {0.015625, -0.234375}
-	},
-	recharging_animation =
-	{
-		filename = "__base__/graphics/entity/roboport/roboport-recharging.png",
-		priority = "high",
-		width = 37,
-		height = 35,
-		frame_count = 16,
-		scale = 1.5,
-		animation_speed = 0.5
-	},
-	vehicle_impact_sound = {filename = "__base__/sound/car-metal-impact.ogg", volume = 0.65},
-	working_sound =
-	{
-		sound = {filename = "__base__/sound/roboport-working.ogg", volume = 0.6},
-		max_sounds_per_type = 3,
-		audible_distance_modifier = 0.5,
-		probability = 1 / (5 * 60)
-	},
-	recharging_light = {intensity = 0.4, size = 5, color = {r = 1.0, g = 1.0, b = 1.0}},
-	request_to_open_door_timeout = 15,
-	spawn_and_station_height = -0.1,
-	draw_logistic_radius_visualization = true,
-	draw_construction_radius_visualization = true,
-	open_door_trigger_effect =
-	{
-		{type = "play-sound", sound = {filename = "__base__/sound/roboport-door.ogg", volume = 1.2}},
-	},
-	close_door_trigger_effect =
-	{
-		{type = "play-sound", sound = {filename = "__base__/sound/roboport-door.ogg", volume = 0.75}},
-	},
-	circuit_wire_connection_point =
-	{
-		shadow =
-		{
-			red = {1.17188, 1.98438},
-			green = {1.04688, 2.04688}
+	base = {
+		layers = {
+			{
+				filename = "__xander-mod__/graphics/entity/logistic/roboport-2/base.png",
+				hr_version = xmutil.NIL,
+			},
 		},
-		wire =
-		{
-			red = {0.78125, 1.375},
-			green = {0.78125, 1.53125}
-		}
 	},
-	circuit_connector_sprites = get_circuit_connector_sprites({0.59375, 1.3125}, nil, 18),
-	circuit_wire_max_distance = 9,
-	default_available_logistic_output_signal = {type = "virtual", name = "signal-X"},
-	default_total_logistic_output_signal = {type = "virtual", name = "signal-Y"},
-	default_available_construction_output_signal = {type = "virtual", name = "signal-Z"},
-	default_total_construction_output_signal = {type = "virtual", name = "signal-T"},
+	base_patch = {
+		filename = "__xander-mod__/graphics/entity/logistic/roboport-2/base-patch.png",
+		hr_version = xmutil.NIL,
+	},
+	base_animation = {
+		filename = "__xander-mod__/graphics/entity/logistic/roboport-2/base-animation.png",
+		hr_version = xmutil.NIL,
+	},
+	door_animation_up = {
+		filename = "__xander-mod__/graphics/entity/logistic/roboport-2/door-up.png",
+		hr_version = xmutil.NIL,
+	},
+	door_animation_down = {
+		filename = "__xander-mod__/graphics/entity/logistic/roboport-2/door-down.png",
+		hr_version = xmutil.NIL,
+	},
 }
+),
 --Base "wooden-chest" placeholder
 --Base "iron-chest" placeholder
 --Base "steel-chest" placeholder
