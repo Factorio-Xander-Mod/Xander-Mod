5bd918bb9677c4c73acba19ea7ded68b1370fdd0
mspielberg <9729801+mspielberg@users.noreply.github.com>
2017-12-17 00:02:42 -0800

    fix tier 2 robots

--- prototypes/entity/logistic/robot.lua
+++ prototypes/entity/logistic/robot.lua
@@ -21,294 +21,65 @@ data:extend(
 
 --Base "logistic-robot" placeholder
 --Fast Logistic Robot
-{
-	type = "logistic-robot",
-	name = "logistic-robot-2",
-	icon = "__xander-mod__/graphics/item/logistic/robot/logistic-robot-2.png",
-	flags = {"placeable-player", "player-creation", "placeable-off-grid", "not-on-map"},
-	minable = {hardness = 0.1, mining_time = 0.1, result = "logistic-robot-2"},
-	resistances = {{type = "fire", percent = 85}},
-	max_health = 150,
-	collision_box = {{0, 0}, {0, 0}},
-	selection_box = {{-0.5, -1.5}, {0.5, -0.5}},
-	max_payload_size = 4,
-	speed = 0.2,
-	transfer_distance = 0.5,
-	max_energy = "5MJ",
-	energy_per_tick = "0.02kJ",
-	speed_multiplier_when_out_of_energy = 0.2,
-	energy_per_move = "10kJ",
-	min_to_charge = 0.2,
-	max_to_charge = 0.95,
-	idle =
+xmutil.clone("logistic-robot", "logistic-robot",
 	{
-		filename = "__xander-mod__/graphics/entity/logistic/logistic-robot-2/2.png",
-		priority = "high",
-		line_length = 16,
-		width = 41,
-		height = 42,
-		frame_count = 1,
-		shift = {0.015625, -0.09375},
-		direction_count = 16,
-		y = 42
 	},
-	idle_with_cargo =
 	{
-		filename = "__xander-mod__/graphics/entity/logistic/logistic-robot-2/2.png",
-		priority = "high",
-		line_length = 16,
-		width = 41,
-		height = 42,
-		frame_count = 1,
-		shift = {0.015625, -0.09375},
-		direction_count = 16
-	},
-	in_motion =
-	{
-		filename = "__xander-mod__/graphics/entity/logistic/logistic-robot-2/2.png",
-		priority = "high",
-		line_length = 16,
-		width = 41,
-		height = 42,
-		frame_count = 1,
-		shift = {0.015625, -0.09375},
-		direction_count = 16,
-		y = 126
-	},
-	in_motion_with_cargo =
-	{
-		filename = "__xander-mod__/graphics/entity/logistic/logistic-robot-2/2.png",
-		priority = "high",
-		line_length = 16,
-		width = 41,
-		height = 42,
-		frame_count = 1,
-		shift = {0.015625, -0.09375},
-		direction_count = 16,
-		y = 84
-	},
-	shadow_idle =
-	{
-		filename = "__base__/graphics/entity/logistic-robot/logistic-robot-shadow.png",
-		priority = "high",
-		line_length = 16,
-		width = 59,
-		height = 23,
-		frame_count = 1,
-		shift = {0.96875, 0.609375},
-		direction_count = 16,
-		y = 23
-	},
-	shadow_idle_with_cargo =
-	{
-		filename = "__base__/graphics/entity/logistic-robot/logistic-robot-shadow.png",
-		priority = "high",
-		line_length = 16,
-		width = 59,
-		height = 23,
-		frame_count = 1,
-		shift = {0.96875, 0.609375},
-		direction_count = 16
-	},
-	shadow_in_motion =
-	{
-		filename = "__base__/graphics/entity/logistic-robot/logistic-robot-shadow.png",
-		priority = "high",
-		line_length = 16,
-		width = 59,
-		height = 23,
-		frame_count = 1,
-		shift = {0.96875, 0.609375},
-		direction_count = 16,
-		y = 23
-	},
-	shadow_in_motion_with_cargo =
-	{
-		filename = "__base__/graphics/entity/logistic-robot/logistic-robot-shadow.png",
-		priority = "high",
-		line_length = 16,
-		width = 59,
-		height = 23,
-		frame_count = 1,
-		shift = {0.96875, 0.609375},
-		direction_count = 16
-	},
-	working_sound = flying_robot_sounds(),
-	cargo_centered = {0.0, 0.2},
-},
+		name = "logistic-robot-2",
+		icon = "__xander-mod__/graphics/item/logistic/robot/logistic-robot-2.png",
+		minable = {hardness = 0.1, mining_time = 0.1, result = "logistic-robot-2"},
+		max_health = 150,
+		max_payload_size = 4,
+		speed = 0.2,
+		max_energy = "5MJ",
+		energy_per_tick = "0.02kJ",
+		energy_per_move = "10kJ",
+		idle = {
+			filename = "__xander-mod__/graphics/entity/logistic/logistic-robot-2/2.png",
+			hr_version = xmutil.NIL,
+		},
+		idle_with_cargo = {
+			filename = "__xander-mod__/graphics/entity/logistic/logistic-robot-2/2.png",
+			hr_version = xmutil.NIL,
+		},
+		in_motion = {
+			filename = "__xander-mod__/graphics/entity/logistic/logistic-robot-2/2.png",
+			hr_version = xmutil.NIL,
+		},
+		in_motion_with_cargo = {
+			filename = "__xander-mod__/graphics/entity/logistic/logistic-robot-2/2.png",
+			hr_version = xmutil.NIL,
+		},
+	}
+),
 --Base "construction-robot" placeholder
 --Durable Construction Robot
-{
-	type = "construction-robot",
-	name = "construction-robot-2",
-	icon = "__xander-mod__/graphics/item/logistic/robot/construction-robot-2.png",
-	flags = {"placeable-player", "player-creation", "placeable-off-grid", "not-on-map"},
-	minable = {hardness = 0.1, mining_time = 0.1, result = "construction-robot-2"},
-	resistances = {{type = "fire", percent = 85}},
-	max_health = 400,
-	collision_box = {{0, 0}, {0, 0}},
-	selection_box = {{-0.5, -1.5}, {0.5, -0.5}},
-	max_payload_size = 2,
-	speed = 0.1,
-	transfer_distance = 0.5,
-	max_energy = "5MJ",
-	energy_per_tick = "0.05kJ",
-	speed_multiplier_when_out_of_energy = 0.2,
-	energy_per_move = "10kJ",
-	min_to_charge = 0.2,
-	max_to_charge = 0.95,
-	working_light = {intensity = 0.8, size = 3, color = {r = 0.8, g = 0.8, b = 0.8}},
-	idle =
+xmutil.clone("construction-robot", "construction-robot",
+	{},
 	{
-		filename = "__xander-mod__/graphics/entity/logistic/construction-robot-2/2.png",
-		priority = "high",
-		line_length = 16,
-		width = 32,
-		height = 36,
-		frame_count = 1,
-		shift = {0, -0.15625},
-		direction_count = 16
-	},
-	in_motion =
-	{
-		filename = "__xander-mod__/graphics/entity/logistic/construction-robot-2/2.png",
-		priority = "high",
-		line_length = 16,
-		width = 32,
-		height = 36,
-		frame_count = 1,
-		shift = {0, -0.15625},
-		direction_count = 16,
-		y = 36
-	},
-	shadow_idle =
-	{
-		filename = "__base__/graphics/entity/construction-robot/construction-robot-shadow.png",
-		priority = "high",
-		line_length = 16,
-		width = 50,
-		height = 24,
-		frame_count = 1,
-		shift = {1.09375, 0.59375},
-		direction_count = 16
-	},
-	shadow_in_motion =
-	{
-		filename = "__base__/graphics/entity/construction-robot/construction-robot-shadow.png",
-		priority = "high",
-		line_length = 16,
-		width = 50,
-		height = 24,
-		frame_count = 1,
-		shift = {1.09375, 0.59375},
-		direction_count = 16
-	},
-	working =
-	{
-		filename = "__xander-mod__/graphics/entity/logistic/construction-robot-2/working.png",
-		priority = "high",
-		line_length = 2,
-		width = 28,
-		height = 36,
-		frame_count = 2,
-		shift = {0, -0.15625},
-		direction_count = 16,
-		animation_speed = 0.3,
-	},
-	shadow_working =
-	{
-		stripes = util.multiplystripes(2,
-		{
-			{
-				filename = "__base__/graphics/entity/construction-robot/construction-robot-shadow.png",
-				width_in_frames = 16,
-				height_in_frames = 1,
-			}
-		}),
-		priority = "high",
-		width = 50,
-		height = 24,
-		frame_count = 2,
-		shift = {1.09375, 0.59375},
-		direction_count = 16
-	},
-	smoke =
-	{
-		filename = "__base__/graphics/entity/smoke-construction/smoke-01.png",
-		width = 39,
-		height = 32,
-		frame_count = 19,
-		line_length = 19,
-		shift = {0.078125, -0.15625},
-		animation_speed = 0.3,
-	},
-	sparks =
-	{
-		{
-			filename = "__base__/graphics/entity/sparks/sparks-01.png",
-			width = 39,
-			height = 34,
-			frame_count = 19,
-			line_length = 19,
-			shift = {-0.109375, 0.3125},
-			tint = { r = 1.0, g = 0.9, b = 0.0, a = 1.0 },
-			animation_speed = 0.3
+		name = "construction-robot-2",
+		icon = "__xander-mod__/graphics/item/logistic/robot/construction-robot-2.png",
+		minable = {hardness = 0.1, mining_time = 0.1, result = "construction-robot-2"},
+		max_health = 400,
+		max_payload_size = 2,
+		speed = 0.1,
+		max_energy = "5MJ",
+		energy_per_tick = "0.05kJ",
+		energy_per_move = "10kJ",
+		idle = {
+			filename = "__xander-mod__/graphics/entity/logistic/construction-robot-2/2.png",
+			hr_version = xmutil.NIL,
 		},
-		{
-			filename = "__base__/graphics/entity/sparks/sparks-02.png",
-			width = 36,
-			height = 32,
-			frame_count = 19,
-			line_length = 19,
-			shift = {0.03125, 0.125},
-			tint = { r = 1.0, g = 0.9, b = 0.0, a = 1.0 },
-			animation_speed = 0.3
+		in_motion = {
+			filename = "__xander-mod__/graphics/entity/logistic/construction-robot-2/2.png",
+			hr_version = xmutil.NIL,
 		},
-		{
-			filename = "__base__/graphics/entity/sparks/sparks-03.png",
-			width = 42,
-			height = 29,
-			frame_count = 19,
-			line_length = 19,
-			shift = {-0.0625, 0.203125},
-			tint = { r = 1.0, g = 0.9, b = 0.0, a = 1.0 },
-			animation_speed = 0.3
+		working = {
+			filename = "__xander-mod__/graphics/entity/logistic/construction-robot-2/working.png",
+			hr_version = xmutil.NIL,
 		},
-		{
-			filename = "__base__/graphics/entity/sparks/sparks-04.png",
-			width = 40,
-			height = 35,
-			frame_count = 19,
-			line_length = 19,
-			shift = {-0.0625, 0.234375},
-			tint = { r = 1.0, g = 0.9, b = 0.0, a = 1.0 },
-			animation_speed = 0.3
-		},
-		{
-			filename = "__base__/graphics/entity/sparks/sparks-05.png",
-			width = 39,
-			height = 29,
-			frame_count = 19,
-			line_length = 19,
-			shift = {-0.109375, 0.171875},
-			tint = { r = 1.0, g = 0.9, b = 0.0, a = 1.0 },
-			animation_speed = 0.3
-		},
-		{
-			filename = "__base__/graphics/entity/sparks/sparks-06.png",
-			width = 44,
-			height = 36,
-			frame_count = 19,
-			line_length = 19,
-			shift = {0.03125, 0.3125},
-			tint = { r = 1.0, g = 0.9, b = 0.0, a = 1.0 },
-			animation_speed = 0.3
-		},
-	},
-	working_sound = flying_robot_sounds(),
-	cargo_centered = {0.0, 0.2},
-	construction_vector = {0.30, 0.22},
-},
+	}
+),
 --Base "roboport" placeholder
 --Long-Range Roboport
 xmutil.clone("roboport", "roboport",
