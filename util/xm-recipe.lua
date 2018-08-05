c5584c0126ca847546fc8d8070a8a8ad8eed54e8
unknown <32825871+Repofme1@users.noreply.github.com>
2018-08-05 18:56:45 -0400

    Tweak to xm_recipe after adding recipe refactor from mspielberg.

--- util/xm-recipe.lua
+++ util/xm-recipe.lua
@@ -1,8 +1,8 @@
 --Input parameters: name	icon	category	tint	time	enabled	ingredients	results	subgroup
 
-function xm_recipe(recipe_name, icon_path, category, tint, crafting_time, enabled, ingredients, results, subgroup)
+function xm_recipe(name, icon_path, category, tint, time, enabled, ingredients, results, subgroup)
 	--Create a list for output recipe parameters
-	local out = {type = "recipe", name = recipe_name}
+	local out = {type = "recipe", name = name}
 	--Set the recipe icon.  Use a custom path if given, otherwise let the base game fill in the icon of the output item
 	out.icon = icon_path
 	out.icon_size = 32
@@ -11,7 +11,7 @@ function xm_recipe(recipe_name, icon_path, category, tint, crafting_time, enable
 	--Set the crafting macine tint if one is given
 	out.crafting_machine_tint = tint
 	--Set the time required for the recipe (base game calls it "energy required")
-	out.energy_required = crafting_time
+	out.energy_required = time
 	--Set whether the recipe is enabled at the start of the game, or requires research to unlock
 	out.enabled = enabled
 	--Set the ingredients list
